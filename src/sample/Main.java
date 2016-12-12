7e0df77f3b8a40130681322b02d5bacdd385242b
Bert <bert2030@gmail.com>
2016-12-12 21:27:35 +0100

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        GridPane root = new GridPane();
+        primaryStage.setTitle("Snake");
+        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
